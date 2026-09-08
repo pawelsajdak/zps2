c5daaaeed7609318eae8a90ff6f2fe6b9b4844a6
p.sajdak <p.sajdak@student.uw.edu.pl>
2026-09-08 20:58:02 +0200

    dodane licencje, uzupełnione opisy plików

--- python/linefit.py
+++ python/linefit.py
@@ -1,10 +1,29 @@
+'''
+  * Copyright (c) 2026 Paweł Sajdak, Franciszek Drągowski
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+'''
+
 #!/usr/bin/python3
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
 
-# Wersja odpowiednia do symulacji zapisującej tylko ekstrema (wersja main.cpp)
+# Program do analizy wyników programu symulacyjnego "main"
+# Przy wywołaniu należy podać nazwę pliku z wynikami znajdującego się w folderze "build"
+
 # Program wybiera minima r, czyli te z ujemnym czasem
 # Do rozkładu r(t) dopasowywana jest prosta 
 # Rysowany jest wykres r(t) oraz dopasowana prosta
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,37 @@
+/*
+  * Copyright (c) 2026 Paweł Sajdak, Franciszek Drągowski
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+///////////////////////////////////////////////
+
+
+// Program przeprowadza symulację wykorzystującą algorytm Rungego-Kutty 4. rzędu
+// Do pliku wyjściowego wypisywane są czasy i separacje (r) dla minimów i ekstremów r
+// Czas wystąpienia minimum r zapisywany jest jako liczba ujemna
+// Symulacja jest domyślnie przeprowadzana uwzględniając poprawki do rzędu 2,5PN
+// Podanie "to2PN = true" powoduje nieuwzględnianie rzędu 2,5PN (poprawki stają się uwzględniane tylko do rzędu 2PN)
+
+// Wywołanie:
+// main <całkowity czas> <krok czasowy> [to2PN]
+
+
 #include <iostream>
 #include <iomanip>
 #include "stdlib.h"
 #include "Simulation.h"
 
-// Wywołanie:
-// main <całkowity czas> <krok czasowy> [to2PN]
 
 int main(int argc, char* argv[]){    
     if (argc < 3){
